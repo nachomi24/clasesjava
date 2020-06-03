3f39f4b4d1c1370285884cc05cbd780723672c2b
nachomi24 <66391621+nachomi24@users.noreply.github.com>
2020-06-03 18:13:44 -0500

    firstclass

---
+++ Clase1/firstclass.java
@@ -0,0 +1,28 @@
+public static void main (String Args[]){
+    System.out.println("If you´re reading this, hi")
+
+    //Declarar variables
+
+    //ints
+    int integerName= 2;
+    //strings
+    String stringName="Holaa";
+    //booleans
+    boolean isVerdad = false; //false
+    //doubles
+    double valorDecimal=0.0;
+
+    System.out.println(integerName+ "" + stringName);
+
+    //Condiciones
+    if ( !(isVerdad))//Negando una condición
+    System.out.println("Valor decimal: " + valorDecimal);
+}else if (integerNmae == 0 /*como comparamos, le ponemos doble igual)*/){
+    System.out.println("El int vale 2: ");
+}else{
+    Systema.out.println("Else!: ");
+}
+}
+
+    }
+}
